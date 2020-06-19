e94a8c8b2ce280ce4df32d7e169cb1fa891ef8a1
Hank Kung <hank910140@gmail.com>
2020-06-20 04:18:29 +0800

    dl

--- predict.py
+++ predict.py
@@ -23,6 +23,20 @@ def generate(name):
     print('Number of sessions({}): {}'.format(name, len(hdfs)))
     return hdfs
 
+def generate_bgl(name, split, window_size):
+    num_sessions = 0
+    bgl = []
+    with open(name+'/window_' + str(window_size) +'future_1/'+ split, 'r') as f_len:
+        file_len = len(f_len.readlines())
+    with open(name+'/window_'+ str(window_size) + 'future_1/'+ split, 'r') as f:
+        for line in f.readlines():
+            num_sessions += 1
+            line = tuple(map(lambda n: n, map(int, line.strip().split())))
+            bgl.append(line)
+            
+    print('Number of sessions({}): {}'.format(name, num_sessions))
+    return bgl
+
 
 class DL(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_keys):
@@ -133,9 +147,7 @@ class Train_Att(nn.Module):
 if __name__ == '__main__':
 
     # Hyperparameters
-    num_classes = 28
     input_size = 1
-    model_path = 'model/Adam_batch_size=2048_epoch=300.pt'
     parser = argparse.ArgumentParser()
     parser.add_argument('-num_layers', default=2, type=int)
     parser.add_argument('-hidden_size', default=64, type=int)
@@ -143,6 +155,9 @@ if __name__ == '__main__':
     parser.add_argument('-num_candidates', default=9, type=int)
     parser.add_argument('-model', type=str, default='dl', \
         choices=['dl', 'att', 'train_att'])
+    parser.add_argument('-dataset', type=str, default='hd', \
+        choices=['hd', 'bgl'])
+    parser.add_argument('-epoch', default=300, type=int)
 
     args = parser.parse_args()
     num_layers = args.num_layers
@@ -151,6 +166,18 @@ if __name__ == '__main__':
     num_candidates = args.num_candidates
     # model_path = 'model/Adam_batch_size=2048_epoch=300'
     # model_path = model_path + '_' + args.model + '.pt'
+    log = 'model/num_layer='+str(num_layers)+'_window_size='+str(window_size)+\
+    '_hidden='+str(hidden_size)+'_dataset='+args.dataset+'_epoch='+str(args.epoch)
+    log = log + '_' + args.model + '.pt'
+
+    if args.dataset == 'hd':
+        test_normal_loader = generate('hdfs_test_normal')
+        test_abnormal_loader = generate('hdfs_test_abnormal')
+        num_classes = 28
+    elif args.dataset == 'bgl':
+        test_normal_loader = generate_bgl('bgl', 'normal_test.txt', window_size)
+        test_abnormal_loader = generate_bgl('bgl', 'abnormal_test.txt', window_size)
+        num_classes = 1834
 
     if args.model == 'dl':
         model = DL(input_size, hidden_size, num_layers, num_classes)
@@ -160,45 +187,74 @@ if __name__ == '__main__':
         model = Train_Att(input_size, hidden_size, num_layers, num_classes)
     model = model.to(device)
 
-    log = 'model/window_size='+str(window_size)
-    log = log + '_' + args.model + '.pt'
+
 
     model.load_state_dict(torch.load(log))
     model.eval()
-    print('model_path: {}'.format(model_path))
-    test_normal_loader = generate('hdfs_test_normal')
-    test_abnormal_loader = generate('hdfs_test_abnormal')
+
+
     TP = 0
     FP = 0
     # Test the model
     tbar = tqdm(test_normal_loader)
-    with torch.no_grad():
-        for index, line in enumerate(tbar):
-            tbar.set_description('')
-            for i in range(len(line) - window_size):
-                seq = line[i:i + window_size]
-                label = line[i + window_size]
+    if args.dataset == 'hd':
+        with torch.no_grad():
+            for index, line in enumerate(tbar):
+                tbar.set_description('')
+                for i in range(len(line) - window_size):
+                    seq = line[i:i + window_size]
+                    label = line[i + window_size]
+                    seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
+                    label = torch.tensor(label).view(-1).to(device)
+                    output = model(seq)
+                    predicted = torch.argsort(output, 1)[0][-num_candidates:]
+                    if label not in predicted:
+                        FP += 1
+                        break
+
+    elif args.dataset == 'bgl':
+        with torch.no_grad():
+            for index, line in enumerate(tbar):
+                tbar.set_description('')
+                seq = line[:window_size]
+                label = line[-1]
                 seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
                 label = torch.tensor(label).view(-1).to(device)
                 output = model(seq)
                 predicted = torch.argsort(output, 1)[0][-num_candidates:]
                 if label not in predicted:
                     FP += 1
-                    break
-    tbar = tqdm(test_abnormal_loader)
-    with torch.no_grad():
-        for index, line in enumerate(tbar):
-            tbar.set_description('')
-            for i in range(len(line) - window_size):
-                seq = line[i:i + window_size]
-                label = line[i + window_size]
+
+
+    if args.dataset == 'hd':
+        tbar = tqdm(test_abnormal_loader)
+        with torch.no_grad():
+            for index, line in enumerate(tbar):
+                tbar.set_description('')
+                for i in range(len(line) - window_size):
+                    seq = line[i:i + window_size]
+                    label = line[i + window_size]
+                    seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
+                    label = torch.tensor(label).view(-1).to(device)
+                    output = model(seq)
+                    predicted = torch.argsort(output, 1)[0][-num_candidates:]
+                    if label not in predicted:
+                        TP += 1
+                        break
+    elif args.dataset == 'bgl':
+        tbar = tqdm(test_abnormal_loader)
+        with torch.no_grad():
+            for index, line in enumerate(tbar):
+                tbar.set_description('')
+                seq = line[:window_size]
+                label = line[-1]
                 seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
                 label = torch.tensor(label).view(-1).to(device)
                 output = model(seq)
                 predicted = torch.argsort(output, 1)[0][-num_candidates:]
                 if label not in predicted:
                     TP += 1
-                    break
+
     # Compute precision, recall and F1-measure
     FN = len(test_abnormal_loader) - TP
     P = 100 * TP / (TP + FP)
--- train.py
+++ train.py
@@ -29,6 +29,28 @@ def generate(name):
     dataset = TensorDataset(torch.tensor(inputs, dtype=torch.float), torch.tensor(outputs))
     return dataset
 
+def generate_bgl(name, window_size):
+    num_sessions = 0
+    inputs = []
+    outputs = []
+    num_keys = set()
+    with open(name+'/window_' + str(window_size) +'future_1/normal_train.txt', 'r') as f_len:
+        file_len = len(f_len.readlines())
+    with open(name+'/window_'+ str(window_size) + 'future_1/normal_train.txt', 'r') as f:
+        for line in f.readlines():
+            num_sessions += 1
+            line = tuple(map(lambda n: n, map(int, line.strip().split())))
+            inputs.append(line[:window_size])
+            outputs.append(line[-1])
+            for key in line:
+                num_keys.add(key)
+
+
+    print('Number of sessions({}): {}'.format(name, num_sessions))
+    print('number of keys:{}'.format(len(num_keys)))
+    dataset = TensorDataset(torch.tensor(inputs, dtype=torch.float), torch.tensor(outputs))
+    return dataset
+
 class DL(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_keys):
         super(DL, self).__init__()
@@ -139,7 +161,7 @@ class Train_Att(nn.Module):
 if __name__ == '__main__':
 
     # Hyperparameters
-    num_classes = 28
+    
     num_epochs = 300
     batch_size = 2048
     input_size = 1
@@ -151,12 +173,24 @@ if __name__ == '__main__':
     parser.add_argument('-window_size', default=10, type=int)
     parser.add_argument('-model', type=str, default='dl', \
         choices=['dl', 'att', 'train_att'])
+    parser.add_argument('-dataset', type=str, default='hd', \
+        choices=['hd', 'bgl'])
+    parser.add_argument('-epoch', default=300, type=int)
     args = parser.parse_args()
     num_layers = args.num_layers
     hidden_size = args.hidden_size
     window_size = args.window_size
-    log = 'window_size='+(str(window_size))
+    num_epochs = args.epoch
+    log = 'num_layer='+str(num_layers)+'_window_size='+str(window_size)+\
+    '_hidden='+str(hidden_size)+'_dataset='+args.dataset+'_epoch='+str(args.epoch)
     log = log + '_' + args.model
+
+    if args.dataset == 'hd':
+        seq_dataset = generate('hdfs_train')
+        num_classes = 28
+    elif args.dataset == 'bgl':
+        seq_dataset = generate_bgl('bgl', window_size)
+        num_classes = 1834
 
     if args.model == 'dl':
         model = DL(input_size, hidden_size, num_layers, num_classes)
@@ -164,9 +198,9 @@ if __name__ == '__main__':
         model = Att(input_size, hidden_size, num_layers, num_classes)
     elif args.model == 'train_att':
         model = Train_Att(input_size, hidden_size, num_layers, num_classes)
+
     model = model.to(device)
 
-    seq_dataset = generate('hdfs_train')
     dataloader = DataLoader(seq_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
     writer = SummaryWriter(log_dir='log/' + log)
 
@@ -179,7 +213,7 @@ if __name__ == '__main__':
     for epoch in range(num_epochs):  # Loop over the dataset multiple times
         train_loss = 0
         tbar = tqdm(dataloader)
-        for step, (seq, label) in enumerate(dataloader):
+        for step, (seq, label) in enumerate(tbar):
             # Forward pass
             seq = seq.clone().detach().view(-1, window_size, input_size).to(device)
             output = model(seq)
@@ -190,7 +224,7 @@ if __name__ == '__main__':
             loss.backward()
             train_loss += loss.item()
             optimizer.step()
-            writer.add_graph(model, seq)
+            # writer.add_graph(model, seq)
             tbar.set_description('Train loss: %.3f' % (train_loss / (step + 1)))
         print('Epoch [{}/{}], train_loss: {:.4f}'.format(epoch + 1, num_epochs, train_loss / total_step))
         writer.add_scalar('train_loss', train_loss / total_step, epoch + 1)
