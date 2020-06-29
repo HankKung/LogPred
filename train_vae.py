105c64ee287b3c96328b92003dbd673f00fbc8a1
Hank Kung <hank910140@gmail.com>
2020-06-30 01:54:56 +0800

    vae

---
+++ train_vae.py
@@ -0,0 +1,137 @@
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import TensorDataset, DataLoader
+import argparse
+from tqdm import tqdm
+import os
+from vae.vae import VRAE
+# Device configuration
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def generate_bgl(window_size):
+    num_sessions = 0
+    inputs = []
+    outputs = []
+    num_keys = set()
+    with open('bgl/window_'+str(window_size)+'future_0/normal_train.txt', 'r') as f_len:
+        file_len = len(f_len.readlines())
+    with open('bgl/window_'+str(window_size)+'future_0/normal_train.txt', 'r') as f:
+        for line in f.readlines():
+            num_sessions += 1
+            line = tuple(map(lambda n: n, map(int, line.strip().split())))
+            inputs.append(line)
+            outputs.append(line)
+            for key in line:
+                num_keys.add(key)
+    print('Number of sessions: {}'.format(num_sessions))
+    print('number of keys:{}'.format(len(num_keys)))
+    dataset = TensorDataset(torch.tensor(inputs, dtype=torch.float), torch.tensor(outputs))
+    return dataset
+
+def generate_hdfs(window_size):
+    num_sessions = 0
+    inputs = []
+    outputs = []
+    with open('data/hdfs_train', 'r') as f:
+        for line in f.readlines():
+            num_sessions += 1
+            line = tuple(map(lambda n: n - 1, map(int, line.strip().split())))
+            for i in range(len(line) - window_size):
+                inputs.append(line[i:i + window_size])
+                outputs.append(line[i:i + window_size])
+    print('Number of sessions: {}'.format(num_sessions))
+    dataset = TensorDataset(torch.tensor(inputs, dtype=torch.float), torch.tensor(outputs))
+    return dataset
+
+
+
+if __name__ == '__main__':
+
+    # Hyperparameters
+    batch_size = 2048
+    input_size = 1
+    model_dir = 'model'
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-num_layers', default=2, type=int)
+    parser.add_argument('-hidden_size', default=128, type=int)
+    parser.add_argument('-window_size', default=20, type=int)
+    parser.add_argument('-dataset', type=str, default='hd', choices=['hd', 'bgl'])
+    parser.add_argument('-epoch', default=150, type=int)
+    parser.add_argument('-lr', default=0.001, type=float)
+    parser.add_argument('-caption', type=str, default='')
+    args = parser.parse_args()
+    num_layers = args.num_layers
+    hidden_size = args.hidden_size
+    window_size = args.window_size
+    num_epochs = args.epoch
+
+    if args.dataset == 'hd':
+        seq_dataset = generate_hdfs(window_size)
+        num_classes = 28
+        # for -1 padding during testing
+        num_classes +=1
+    elif args.dataset == 'bgl':
+        seq_dataset = generate_bgl(window_size)
+        num_classes = 1834
+    
+    dataloader = DataLoader(seq_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
+
+
+    log = 'dataset='+ str(args.dataset) + \
+    '_window_size=' + str(window_size) + \
+    '_hidden_size=' + str(hidden_size) + \
+    '_num_layer=' + str(num_layers) + \
+     '_epoch=' + str(num_epochs)
+    log = log + '_lr=' + str(args.lr) if args.lr != 0.001 else log
+    log = log + '_vae' + args.caption
+    print('store model at:')
+    print(log)
+    writer = SummaryWriter(log_dir='log/' + log)
+
+
+    model = VRAE(sequence_length=window_size,
+            number_of_features = 1,
+            num_classes = num_classes,
+            hidden_size = hidden_size)
+    model = model.to(device)
+
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    # Train the model
+    total_step = len(dataloader)
+    for epoch in range(num_epochs):  # Loop over the dataset multiple times
+        train_loss = 0
+        tbar = tqdm(dataloader)
+        for step, (seq, label) in enumerate(tbar):
+            # Forward pass
+            seq = seq.clone().detach().view(-1, window_size, input_size).to(device)
+            # label = label.to(device)
+            # output = model(seq)
+
+            loss, rec, kl = model.compute_loss(seq)
+
+            # loss = 0
+            # for i in range(window_size):
+            #     loss += criterion(output[:,i,:], label[:,i])
+
+            optimizer.zero_grad()
+            loss.backward()
+            train_loss += loss.item()
+            optimizer.step()
+            tbar.set_description('Train loss: %.3f' % (train_loss / (step + 1)))
+        print('Epoch [{}/{}], train_loss: {:.4f}'.format(epoch + 1, num_epochs, train_loss / total_step))
+        writer.add_scalar('train_loss', train_loss / total_step, epoch + 1)
+
+    if not os.path.isdir(model_dir):
+        os.makedirs(model_dir)
+    torch.save(model.state_dict(), model_dir + '/' + log + '.pt')
+    writer.close()
+    print('Finished Training')
---
+++ vae/vae.py
@@ -0,0 +1,461 @@
+## code modeified from https://github.com/tejaslodaya/timeseries-clustering-vae
+
+import numpy as np
+import torch
+from torch import nn, optim
+from torch import distributions
+from .base import BaseEstimator
+from torch.utils.data import DataLoader
+import os
+
+class Encoder(nn.Module):
+    """
+    Encoder network containing enrolled LSTM/GRU
+    :param number_of_features: number of input features
+    :param hidden_size: hidden size of the RNN
+    :param hidden_layer_depth: number of layers in RNN
+    :param latent_length: latent vector length
+    :param dropout: percentage of nodes to dropout
+    :param block: LSTM/GRU block
+    """
+    def __init__(self, number_of_features, hidden_size, hidden_layer_depth, latent_length, dropout):
+
+        super(Encoder, self).__init__()
+
+        self.number_of_features = number_of_features
+        self.hidden_size = hidden_size
+        self.hidden_layer_depth = hidden_layer_depth
+        self.latent_length = latent_length
+
+        self.model = nn.LSTM(self.number_of_features, self.hidden_size, self.hidden_layer_depth, dropout=dropout, batch_first=True)
+
+
+    def forward(self, x):
+        """Forward propagation of encoder. Given input, outputs the last hidden state of encoder
+        :param x: input to the encoder, of shape (sequence_length, batch_size, number_of_features)
+        :return: last hidden state of encoder, of shape (batch_size, hidden_size)
+        """
+        _, (h_end, c_end) = self.model(x)
+        h_end = h_end[-1, :, :]
+        return h_end
+
+
+class Lambda(nn.Module):
+    """Lambda module converts output of encoder to latent vector
+    :param hidden_size: hidden size of the encoder
+    :param latent_length: latent vector length
+    """
+    def __init__(self, hidden_size, latent_length):
+        super(Lambda, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.latent_length = latent_length
+
+        self.hidden_to_mean = nn.Linear(self.hidden_size, self.latent_length)
+        self.hidden_to_logvar = nn.Linear(self.hidden_size, self.latent_length)
+
+        nn.init.xavier_uniform_(self.hidden_to_mean.weight)
+        nn.init.xavier_uniform_(self.hidden_to_logvar.weight)
+
+    def forward(self, cell_output):
+        """Given last hidden state of encoder, passes through a linear layer, and finds the mean and variance
+        :param cell_output: last hidden state of encoder
+        :return: latent vector
+        """
+
+        self.latent_mean = self.hidden_to_mean(cell_output)
+        self.latent_logvar = self.hidden_to_logvar(cell_output)
+
+        if self.training:
+            std = torch.exp(0.5 * self.latent_logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(self.latent_mean)
+        else:
+            return self.latent_mean
+
+class Decoder(nn.Module):
+    """Converts latent vector into output
+    :param sequence_length: length of the input sequence
+    :param batch_size: batch size of the input sequence
+    :param hidden_size: hidden size of the RNN
+    :param hidden_layer_depth: number of layers in RNN
+    :param latent_length: latent vector length
+    :param output_size: 2, one representing the mean, other log std dev of the output
+    :param block: GRU/LSTM - use the same which you've used in the encoder
+    :param dtype: Depending on cuda enabled/disabled, create the tensor
+    """
+    def __init__(self, sequence_length, batch_size, hidden_size, hidden_layer_depth, latent_length, output_size, dtype):
+
+        super(Decoder, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+        self.sequence_length = sequence_length
+        self.hidden_layer_depth = hidden_layer_depth
+        self.latent_length = latent_length
+        self.output_size = output_size
+        self.dtype = dtype
+
+        self.model = nn.LSTM(1, self.hidden_size, self.hidden_layer_depth)
+
+
+        self.latent_to_hidden = nn.Linear(self.latent_length, self.hidden_size)
+        self.hidden_to_output = nn.Linear(self.hidden_size, self.output_size)
+
+        nn.init.xavier_uniform_(self.latent_to_hidden.weight)
+        nn.init.xavier_uniform_(self.hidden_to_output.weight)
+
+    def forward(self, latent):
+        """Converts latent to hidden to output
+        :param latent: latent vector
+        :return: outputs consisting of mean and std dev of vector
+        """
+        h_state = self.latent_to_hidden(latent)
+        batch_size = h_state.shape[0]
+
+        decoder_inputs = torch.zeros(self.sequence_length, batch_size, 1, requires_grad=True).type(self.dtype)
+        h_0 = torch.stack([h_state for _ in range(self.hidden_layer_depth)])
+        c_0 = torch.zeros(self.hidden_layer_depth, batch_size, self.hidden_size, requires_grad=True).type(self.dtype)
+        decoder_output, _ = self.model(decoder_inputs, (h_0, c_0))
+
+        out = self.hidden_to_output(decoder_output)
+        return out
+
+def _assert_no_grad(tensor):
+    assert not tensor.requires_grad, \
+        "nn criterions don't compute the gradient w.r.t. targets - please " \
+        "mark these tensors as not requiring gradients"
+
+class VRAE(nn.Module):
+    """Variational recurrent auto-encoder. This module is used for dimensionality reduction of timeseries
+    :param sequence_length: length of the input sequence
+    :param number_of_features: number of input features
+    :param hidden_size:  hidden size of the RNN
+    :param hidden_layer_depth: number of layers in RNN
+    :param latent_length: latent vector length
+    :param batch_size: number of timeseries in a single batch
+    :param learning_rate: the learning rate of the module
+    :param block: GRU/LSTM to be used as a basic building block
+    :param n_epochs: Number of iterations/epochs
+    :param dropout_rate: The probability of a node being dropped-out
+    :param optimizer: ADAM/ SGD optimizer to reduce the loss function
+    :param loss: SmoothL1Loss / MSELoss / ReconLoss / any custom loss which inherits from `_Loss` class
+    :param boolean cuda: to be run on GPU or not
+    :param print_every: The number of iterations after which loss should be printed
+    :param boolean clip: Gradient clipping to overcome explosion
+    :param max_grad_norm: The grad-norm to be clipped
+    :param dload: Download directory where models are to be dumped
+    """
+    def __init__(self,
+                 sequence_length, 
+                 number_of_features,
+                 num_classes,
+                 hidden_size=128,
+                 hidden_layer_depth=2,
+                 latent_length=20,
+                 batch_size=2048,
+                 learning_rate=0.005,
+                 n_epochs=50,
+                 dropout_rate=0.,
+                 cuda=True,
+                 print_every=100,
+                 clip=True,
+                 max_grad_norm=5,
+                 dload='.'):
+
+        super(VRAE, self).__init__()
+
+
+        self.dtype = torch.FloatTensor
+        self.use_cuda = cuda
+        self.num_classes = num_classes
+        if not torch.cuda.is_available() and self.use_cuda:
+            self.use_cuda = False
+
+
+        if self.use_cuda:
+            self.dtype = torch.cuda.FloatTensor
+
+
+        self.encoder = Encoder(number_of_features = number_of_features,
+                               hidden_size=hidden_size,
+                               hidden_layer_depth=hidden_layer_depth,
+                               latent_length=latent_length,
+                               dropout=dropout_rate)
+
+        self.lmbd = Lambda(hidden_size=hidden_size,
+                           latent_length=latent_length)
+
+        self.decoder = Decoder(sequence_length=sequence_length,
+                               batch_size = batch_size,
+                               hidden_size=hidden_size,
+                               hidden_layer_depth=hidden_layer_depth,
+                               latent_length=latent_length,
+                               output_size=num_classes,
+                               dtype=self.dtype)
+
+        self.sequence_length = sequence_length
+        self.hidden_size = hidden_size
+        self.hidden_layer_depth = hidden_layer_depth
+        self.latent_length = latent_length
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.n_epochs = n_epochs
+
+        self.print_every = print_every
+        self.clip = clip
+        self.max_grad_norm = max_grad_norm
+        self.is_fitted = False
+        self.dload = dload
+
+        if self.use_cuda:
+            self.cuda()
+
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+
+        self.loss_fn = nn.CrossEntropyLoss() 
+
+    def __repr__(self):
+        return """VRAE(n_epochs={n_epochs},batch_size={batch_size},cuda={cuda})""".format(
+                n_epochs=self.n_epochs,
+                batch_size=self.batch_size,
+                cuda=self.use_cuda)
+
+    def forward(self, x):
+        """
+        Forward propagation which involves one pass from inputs to encoder to lambda to decoder
+        :param x:input tensor
+        :return: the decoded output, latent vector
+        """
+        cell_output = self.encoder(x)
+        latent = self.lmbd(cell_output)
+        x_decoded = self.decoder(latent)
+        return x_decoded, latent
+
+    def _rec(self, x_decoded, x, loss_fn):
+        """
+        Compute the loss given output x decoded, input x and the specified loss function
+        :param x_decoded: output of the decoder
+        :param x: input to the encoder
+        :param loss_fn: loss function specified
+        :return: joint loss, reconstruction loss and kl-divergence loss
+        """
+        latent_mean, latent_logvar = self.lmbd.latent_mean, self.lmbd.latent_logvar
+
+        kl_loss = -0.5 * torch.mean(1 + latent_logvar - latent_mean.pow(2) - latent_logvar.exp())
+        x_decoded = x_decoded.view(-1, x_decoded.shape[0], self.num_classes)
+        x = x.long().squeeze(2)
+        print(x_decoded.shape)
+
+        print(x.shape)
+        recon_loss = loss_fn(x_decoded, x)
+
+        return kl_loss + recon_loss, recon_loss, kl_loss
+
+    def compute_loss(self, X):
+        """
+        Given input tensor, forward propagate, compute the loss, and backward propagate.
+        Represents the lifecycle of a single iteration
+        :param X: Input tensor
+        :return: total loss, reconstruction loss, kl-divergence loss and original input
+        """
+        x = torch.tensor(X[:,:,:].type(self.dtype), requires_grad = True)
+
+        x_decoded, _ = self(x)
+        loss, recon_loss, kl_loss = self._rec(x_decoded, x.detach(), self.loss_fn)
+
+        return loss, recon_loss, kl_loss
+
+
+    def _train(self, train_loader):
+        """
+        For each epoch, given the batch_size, run this function batch_size * num_of_batches number of times
+        :param train_loader:input train loader with shuffle
+        :return:
+        """
+        self.train()
+
+        epoch_loss = 0
+        t = 0
+
+        for t, X in enumerate(train_loader):
+
+            # Index first element of array to return tensor
+            X = X[0]
+
+            # required to swap axes, since dataloader gives output in (batch_size x seq_len x num_of_features)
+            X = X.permute(1,0,2)
+
+            self.optimizer.zero_grad()
+            loss, recon_loss, kl_loss, _ = self.compute_loss(X)
+            loss.backward()
+
+            if self.clip:
+                torch.nn.utils.clip_grad_norm_(self.parameters(), max_norm = self.max_grad_norm)
+
+            # accumulator
+            epoch_loss += loss.item()
+
+            self.optimizer.step()
+
+            if (t + 1) % self.print_every == 0:
+                print('Batch %d, loss = %.4f, recon_loss = %.4f, kl_loss = %.4f' % (t + 1, loss.item(),
+                                                                                    recon_loss.item(), kl_loss.item()))
+
+        print('Average loss: {:.4f}'.format(epoch_loss / t))
+
+
+    def fit(self, dataset, save = False):
+        """
+        Calls `_train` function over a fixed number of epochs, specified by `n_epochs`
+        :param dataset: `Dataset` object
+        :param bool save: If true, dumps the trained model parameters as pickle file at `dload` directory
+        :return:
+        """
+
+        train_loader = DataLoader(dataset = dataset,
+                                  batch_size = self.batch_size,
+                                  shuffle = True,
+                                  drop_last=True)
+
+        for i in range(self.n_epochs):
+            print('Epoch: %s' % i)
+
+            self._train(train_loader)
+
+        self.is_fitted = True
+        if save:
+            self.save('model.pth')
+
+
+    def _batch_transform(self, x):
+        """
+        Passes the given input tensor into encoder and lambda function
+        :param x: input batch tensor
+        :return: intermediate latent vector
+        """
+        return self.lmbd(
+                    self.encoder(
+                        Torch.tensor(x.type(self.dtype), requires_grad = False)
+                    )
+        ).cpu().data.numpy()
+
+    def _batch_reconstruct(self, x):
+        """
+        Passes the given input tensor into encoder, lambda and decoder function
+        :param x: input batch tensor
+        :return: reconstructed output tensor
+        """
+
+        x = Torch.tensor(x.type(self.dtype), requires_grad = False)
+        x_decoded, _ = self(x)
+
+        return x_decoded.cpu().data.numpy()
+
+    def reconstruct(self, dataset, save = False):
+        """
+        Given input dataset, creates dataloader, runs dataloader on `_batch_reconstruct`
+        Prerequisite is that model has to be fit
+        :param dataset: input dataset who's output vectors are to be obtained
+        :param bool save: If true, dumps the output vector dataframe as a pickle file
+        :return:
+        """
+
+        self.eval()
+
+        test_loader = DataLoader(dataset = dataset,
+                                 batch_size = self.batch_size,
+                                 shuffle = False,
+                                 drop_last=True) # Don't shuffle for test_loader
+
+        if self.is_fitted:
+            with torch.no_grad():
+                x_decoded = []
+
+                for t, x in enumerate(test_loader):
+                    x = x[0]
+                    x = x.permute(1, 0, 2)
+
+                    x_decoded_each = self._batch_reconstruct(x)
+                    x_decoded.append(x_decoded_each)
+
+                x_decoded = np.concatenate(x_decoded, axis=1)
+
+                if save:
+                    if os.path.exists(self.dload):
+                        pass
+                    else:
+                        os.mkdir(self.dload)
+                    x_decoded.dump(self.dload + '/z_run.pkl')
+                return x_decoded
+
+        raise RuntimeError('Model needs to be fit')
+
+
+    def transform(self, dataset, save = False):
+        """
+        Given input dataset, creates dataloader, runs dataloader on `_batch_transform`
+        Prerequisite is that model has to be fit
+        :param dataset: input dataset who's latent vectors are to be obtained
+        :param bool save: If true, dumps the latent vector dataframe as a pickle file
+        :return:
+        """
+        self.eval()
+
+        test_loader = DataLoader(dataset = dataset,
+                                 batch_size = self.batch_size,
+                                 shuffle = False,
+                                 drop_last=True) # Don't shuffle for test_loader
+        if self.is_fitted:
+            with torch.no_grad():
+                z_run = []
+
+                for t, x in enumerate(test_loader):
+                    x = x[0]
+                    x = x.permute(1, 0, 2)
+
+                    z_run_each = self._batch_transform(x)
+                    z_run.append(z_run_each)
+
+                z_run = np.concatenate(z_run, axis=0)
+                if save:
+                    if os.path.exists(self.dload):
+                        pass
+                    else:
+                        os.mkdir(self.dload)
+                    z_run.dump(self.dload + '/z_run.pkl')
+                return z_run
+
+        raise RuntimeError('Model needs to be fit')
+
+    def fit_transform(self, dataset, save = False):
+        """
+        Combines the `fit` and `transform` functions above
+        :param dataset: Dataset on which fit and transform have to be performed
+        :param bool save: If true, dumps the model and latent vectors as pickle file
+        :return: latent vectors for input dataset
+        """
+        self.fit(dataset, save = save)
+        return self.transform(dataset, save = save)
+
+    def save(self, file_name):
+        """
+        Pickles the model parameters to be retrieved later
+        :param file_name: the filename to be saved as,`dload` serves as the download directory
+        :return: None
+        """
+        PATH = self.dload + '/' + file_name
+        if os.path.exists(self.dload):
+            pass
+        else:
+            os.mkdir(self.dload)
+        torch.save(self.state_dict(), PATH)
+
+    def load(self, PATH):
+        """
+        Loads the model's parameters from the path mentioned
+        :param PATH: Should contain pickle file
+        :return: None
+        """
+        self.is_fitted = True
+        self.load_state_dict(torch.load(PATH))
